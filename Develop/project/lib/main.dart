4637b3fecaa9c9fa1751af83ecddbea35e8fb8b8
minu-dev <88576607+minu-dev@users.noreply.github.com>
2024-09-15 10:52:11 +0000

    By Codespaces

---
+++ Develop/project/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Text(
+          "Hello Flutter",
+          style: TextStyle(fontSize: 28),
+          ),
+      ),
+    );
+  }
+}
